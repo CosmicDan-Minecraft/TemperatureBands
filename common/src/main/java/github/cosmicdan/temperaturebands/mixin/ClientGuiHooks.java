36563e29b343edc0413fd49233595d5a5617d075
CosmicDan <dconnolly.au@gmail.com>
2026-06-29 14:15:58 +1000

    2.0.2: Some fixes [...]  - Solved crash caused by trying to process "fake" levels (from e.g. Supplementaries mod and/or Moonlight Lib)  - Properly handle dimension data for runtime-generated dimensions that re-use the overworld noise router

--- common/src/main/java/github/cosmicdan/temperaturebands/mixin/ClientGuiHooks.java
+++ common/src/main/java/github/cosmicdan/temperaturebands/mixin/ClientGuiHooks.java
@@ -30,7 +30,6 @@ public abstract class ClientGuiHooks {
         )
         private void onPopScreen(CallbackInfo ci) {
             TemperatureBands.clearDimensionDataAndConfig(null);
-            TemperatureBands.logDebug("World creation cancelled, clearing dimension data/config");
         }
     }
 
